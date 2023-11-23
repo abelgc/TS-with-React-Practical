622a25cafc35d200225c6eab6e977cf94ef2e0c1
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-11-23 16:15:49 +0100

    Adding Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import CourseGoal from './components/CourseGoal.tsx';
+import Header from './components/Header.tsx';
+import goalsImg from './assets/goals.jpg';
 
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title="Learn React + TS"
-        description="Learn it from the ground up"
-      />
-    </main>);
+       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <CourseGoal title="Learn React + TS">
+        <p>Learn it from the ground up</p>
+      </CourseGoal>
+    </main>
+  );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,17 +1,19 @@
-interface CourseGoalProps {
-  title: string;
-  description: string
-}
-  
-  
+import { PropsWithChildren } from "react";
 
-export default function CourseGoal({title, description}:CourseGoalProps){
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode
+// }
+  
+type CourseGoalProps = PropsWithChildren<{title: string}>;
+
+export default function CourseGoal({title, children}:CourseGoalProps){
 
     return (
         <article>
           <div>
             <h2>{title}</h2>
-            <p>{description}</p>
+            <p>{children}</p>
           </div>
           <button>Delete</button>
         </article>
