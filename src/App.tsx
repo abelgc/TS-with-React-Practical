3091b739afd9584c588d2882e364d8647a097a26
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-11-24 13:22:05 +0100

    Add handleDelete drilling functions through child components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
-import CourseGoal from './components/CourseGoal.tsx';
+
 import Header from './components/Header.tsx';
 import goalsImg from './assets/goals.jpg';
+import CourseGoalList from './components/CourseGoalList.tsx';
 
-type CourseGoal = {
+export type CourseGoal = {
   title: string;
   description: string;
   id: number;
@@ -16,29 +17,29 @@ export default function App() {
 
   function handleAddGoal() {
 
-    setGoals((prev) => {
+    setGoals((existingGoals) => {
       const newGoal: CourseGoal = {
         id: Math.random(),
         title: "Harcoded Title",
         description: "Harcoded Description",
       };
-      return [...prev, newGoal];
+      return [...existingGoals, newGoal];
     });
   }
 
+  function handleDeleteGoal(id: number) {
+
+    setGoals(existingGoals => existingGoals.filter((goal) => goal.id !== id));
+
+
+  }
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add a Goal</button>
-      <ul>
-        {goals.map((item) => (
-          <li key={item.id}>
-            <CourseGoal title={item.title}>{item.description}</CourseGoal>
-          </li>
-        ))}
-      </ul>
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal}/>
     </main>
   );
 }
