fcb36b59b0690e6cfeafdc66d318ea29bdd1ca38
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-11-28 12:39:16 +0100

    Add useref hoof to AddNewGoal component

--- src/components/AddNewGoal.tsx
+++ src/components/AddNewGoal.tsx
@@ -1,19 +1,27 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 export default function AddNewGoal() {
-    function handleSubmit(event: FormEvent) {
-      event.preventDefault();
+
+    const goal = useRef<HTMLInputElement>(null);
+    const summary = useRef<HTMLInputElement>(null);
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+      event.preventDefault(); //avoid sending http request
+
+      const enteredGoal = goal.current!.value;
+      const enteredSummary = summary.current!.value;
+      
     }
   
     return (
       <form onSubmit={handleSubmit}>
         <p>
           <label htmlFor="goal">Your goal</label>
-          <input id="goal" type="text" />
+          <input id="goal" type="text" ref= {goal} />
         </p>
         <p>
           <label htmlFor="summary">Short summary</label>
-          <input id="summary" type="text" />
+          <input id="summary" type="text" ref= {summary}/>
         </p>
         <p>
           <button>Add Goal</button>
