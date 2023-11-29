f8f9036e2df57823c951aacdd3ef93a6e2293290
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-11-29 11:39:04 +0100

    Fix multi root element in CourseGoalList Component

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from './CourseGoal.tsx';
 import { type CourseGoal as CGoal } from "../App.tsx";
+import { InfoBox } from './InfoBox.tsx';
+import { type ReactNode } from 'react';
 
 type CourseGoalListProps = {
     goals: CGoal[];
@@ -9,16 +11,37 @@ type CourseGoalListProps = {
 
 export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
 
+    if(goals.length === 0) {
+        return (
+            <InfoBox mode="hint">
+              You have no course goals yet. Start adding some!
+            </InfoBox>
+          );
+    }
+    let warningBox : ReactNode;
+
+    if(goals.length >= 4) {
+        warningBox = (
+            <InfoBox mode="warning">
+              You have more than three goals. Consider removing some. Be humble!
+            </InfoBox>
+          );
+    }
+    
     return (
-        <ul>
-            {goals.map((item) => (
-                <li key={item.id}>
-                    <CourseGoal id={item.id} title={item.title} onDelete={onDeleteGoal}>
-                        <p>{item.description}</p>
-                    </CourseGoal>
-                </li>
-            ))}
-        </ul>
+        // root element
+        <>
+        {warningBox}
+            <ul>
+                {goals.map((item) => (
+                    <li key={item.id}>
+                        <CourseGoal id={item.id} title={item.title} onDelete={onDeleteGoal}>
+                            <p>{item.description}</p>
+                        </CourseGoal>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
 
